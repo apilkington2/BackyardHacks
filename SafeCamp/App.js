a99fc06c421579f2d3c8b64e8fa43ce7bfb07f66
Adrian <apilkington@huskers.unl.edu>
2020-05-22 17:29:50 -0500

    finished home page

--- SafeCamp/App.js
+++ SafeCamp/App.js
@@ -14,7 +14,9 @@ import {
   View,
   Text,
   StatusBar,
-  Image
+  Button,
+  Image,
+  ImageBackground
 } from 'react-native';
 import {
   Header,
@@ -26,6 +28,14 @@ import {
 export const BACKGROUND = require('./background.png'); 
 
 const App: () => React$Node = () => {
+
+  // Variables
+  const weather = 'Sunny';
+  const temp = 75;
+  const day = 'Sunday';
+  const uv_index = 6.3;
+  const air_quality = 85;
+
   return (
     <>
       {/* <StatusBar barStyle="dark-content" /> */}
@@ -70,7 +80,27 @@ const App: () => React$Node = () => {
         </ScrollView> */}
       {/* </SafeAreaView> */}
       <View style={styles.body}>
-        <Image source={BACKGROUND} style={styles.backgroundImage}/>
+        <ImageBackground source={BACKGROUND} style={styles.backgroundImage}>
+          <Text style={styles.day}>{day}</Text>
+          <Text style={styles.temperature}>{temp.toString()}</Text>
+          <Text style={styles.weather}>{weather}</Text>
+
+          <View style={styles.right}>
+            <Text style={styles.uv}>UV Index:</Text>
+            <Text style={styles.uvIndex}>{uv_index.toString()}</Text>
+            <Text style={styles.uv}>Air Quality:</Text>
+            <Text style={styles.uvIndex}>{air_quality.toString()}</Text>
+          </View>          
+
+          <View style={styles.mainConatinerStyle}>
+            <Button color="#339966" title="Scan for Poison Ivy"></Button>
+            </View>  
+          <View style={styles.snakesButton}>
+            <Button color="#339966" title="Scan Snakes"></Button>
+          </View>
+                  
+
+        </ImageBackground>
       </View> 
     </>
   );
@@ -80,8 +110,59 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  text: {
+  mainConatinerStyle: {
+    flexDirection: 'column',
+    flex: 1,
+    paddingTop: 140,
+    paddingLeft: '25%',
+    paddingRight: '25%'
+  },
+snakesButton: {
+  paddingLeft: '25%',
+  paddingRight: '25%',
+  paddingBottom: 20
+},
+  poisonIvy: {
+    position: 'absolute',
+    bottom:0,
+  },
+  right: {
+    alignSelf: 'flex-end',
+    marginTop: -165,
+  },
+  uvIndex: {
+    fontFamily: 'Century Gothic 400',
+    fontSize: 35,
     color: Colors.white,
+    paddingLeft: 40
+  },
+  uv: {
+    fontFamily: 'Century Gothic 400',
+    fontSize: 25,
+    color: Colors.white,
+    paddingRight: 30
+  },
+  day: {
+    color: Colors.white,
+    fontSize: 25, 
+    paddingTop: 220,
+    fontFamily: 'Century Gothic 400',
+    paddingLeft: 50,
+  },
+  weather: {
+    color: Colors.white,
+    fontFamily: 'Century Gothic 400',
+    fontSize: 25,
+    paddingLeft: 55,
+  },
+  temperature: {
+    color: Colors.white,
+    fontSize: 110,
+    // textAlign: 'center', // <-- the magic
+    fontFamily: 'Century Gothic 400',
+    paddingLeft: 35,
+    marginTop: -20,
+    marginBottom: -20,
   },
   backgroundImage: {
     width: '100%',
