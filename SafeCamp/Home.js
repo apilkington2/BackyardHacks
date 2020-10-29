387f6d0bd7446ca2913503df15e27e265c3f251c
apilkington2 <pilkadrian6@gmail.com>
2020-10-28 19:38:02 -0500

    Removed unnecessary comment

--- SafeCamp/Home.js
+++ SafeCamp/Home.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
